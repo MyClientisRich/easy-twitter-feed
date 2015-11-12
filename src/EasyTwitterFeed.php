5f658b708183681f824c819d2e41c09f6eae7fd0
Thomas Moreira <thomas@myclientisrich.com>
2015-11-12 15:22:02 +0100

    suppression commentaire

--- src/EasyTwitterFeed.php
+++ src/EasyTwitterFeed.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thomas
- * Date: 12/11/2015
- * Time: 13:35
- */
 
 namespace ETF;
 
--- src/Tweet.php
+++ src/Tweet.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: thomas
- * Date: 12/11/2015
- * Time: 14:43
- */
 
 namespace ETF;
 
